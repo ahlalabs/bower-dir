4d0b1256ad4a014250b44c9b7d4cf20ed8200690
hamad-almamari <alonedream15h@gmail.com>
2017-03-08 16:01:52 +0400

    First release

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var glob        = require('glob');
+var isEmpty     = require('lodash.isempty');
+var bowerdir    = 'bower_components';
+var bowerrcobj  = glob.sync('.bowerrc' , {nodir: true});
+var cwd         = process.cwd();
+var fs          = require('fs');
+
+if( isEmpty( bowerrcobj ) === false ){
+  var bowerrc = JSON.parse( fs.readFileSync( cwd + '/.bowerrc', 'utf8') );
+  if( bowerrc.directory ){
+    bowerdir = bowerrc.directory;
+  }
+}
+
+module.export = bowerdir;
